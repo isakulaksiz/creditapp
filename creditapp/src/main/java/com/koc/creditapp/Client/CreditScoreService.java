c2573dd7d3626a7b3f094926be5d260101d09c28
isakulaksiz <isakulaksiz@outlook.com>
2020-10-27 13:24:56 +0300

    Add CreditScore and Identity is checked

---
+++ creditapp/src/main/java/com/koc/creditapp/Client/CreditScoreService.java
@@ -0,0 +1,23 @@
+package com.koc.creditapp.Client;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CreditScoreService {
+
+    public static final Map<String, BigDecimal> creditScore = new HashMap<>();
+
+    static {
+        creditScore.put("",BigDecimal.valueOf(200));
+        creditScore.put("",BigDecimal.valueOf(400));
+        creditScore.put("",BigDecimal.valueOf(500));
+        creditScore.put("",BigDecimal.valueOf(900));
+    }
+
+    public static BigDecimal getCreditScore(final String identityNo){
+        if(!creditScore.containsKey(identityNo))
+            throw IdentityNoNotFoundException.create(identityNo);
+        return creditScore.get(identityNo);
+    }
+}
---
+++ creditapp/src/main/java/com/koc/creditapp/Client/IdentityNoNotFoundException.java
@@ -0,0 +1,18 @@
+package com.koc.creditapp.Client;
+
+public class IdentityNoNotFoundException extends IllegalArgumentException{
+    public final String message;
+
+    private IdentityNoNotFoundException(String message){
+        this.message= message;
+    }
+
+    public static IdentityNoNotFoundException create(String message) {
+        return new IdentityNoNotFoundException(message);
+    }
+
+    @Override
+    public String getMessage(){
+        return "Credit score not found !"+message;
+    }
+}
